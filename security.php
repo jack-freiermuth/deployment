e4e194e5190076362bcec0dffc6eca4b3c791afc
Jack Freiermuth <jack.freiermuth@gmail.com>
2017-05-11 07:57:01 -0500

    update securty

--- security.php
+++ security.php
@@ -1,14 +1,14 @@
 <?php
-if ( $_SERVER['HTTP_HOST'] != 'localhost' && $_SERVER['HTTP_HOST'] != '127.0.0.1' ) {
-	if(preg_match("((((107\.170\.53\.142)\..*).*)$)", $_SERVER['REMOTE_ADDR']) == 0){
-		echo $_SERVER['REMOTE_ADDR'];
-		die('<!DOCTYPE html>
-			<html>
-				<body>
-					<script type="text/javascript" src="starwolf.js"></script>
-					<pre> Access Denied </pre>
-				</body>
-			</html>
-			');
-	}
+$http_hosts_to_allow = array('localhost', '127.0.0.1');
+$remote_address_to_allow = array('66.41.227.234');
+if ( !in_array($_SERVER['HTTP_HOST'], $http_hosts_to_block) && !in_array($_SERVER['REMOTE_ADDR'], $remote_address_to_allow)) {
+	echo $_SERVER['REMOTE_ADDR'];
+	die('<!DOCTYPE html>
+		<html>
+		<body>
+			<script type="text/javascript" src="starwolf.js"></script>
+			<pre> Access Denied </pre>
+		</body>
+		</html>
+	');
 }
