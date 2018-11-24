8370bd2aa54e6d23fce541a73bcebab742160030
Jack Freiermuth <jack.freiermuth@gmail.com>
2018-11-24 14:41:53 -0600

    new IP

--- security.php
+++ security.php
@@ -1,6 +1,6 @@
 <?php
 $http_hosts_to_allow = array('localhost', '127.0.0.1');
-$remote_address_to_allow = array('66.41.227.234');
+$remote_address_to_allow = array('73.185.138.190');
 if ( !in_array($_SERVER['HTTP_HOST'], $http_hosts_to_allow) && !in_array($_SERVER['REMOTE_ADDR'], $remote_address_to_allow)) {
 	echo $_SERVER['REMOTE_ADDR'];
 	die('<!DOCTYPE html>
